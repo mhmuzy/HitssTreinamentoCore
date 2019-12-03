77ebdd511225682981a380dc5cccf19576b72c2b
marcio henrique <marcio.freitas@globalhitss.com.br>
2019-12-03 17:02:27 -0300

    Inicialização do Migrations - Parte 1

---
+++ Projeto.Data/Mappings/FornecedorMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Projeto.Data.Entities;
+
+namespace Projeto.Data.Mappings
+{
+    // classe de mapeamentos para a entidade Fornecedor
+    public class FornecedorMap //: IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure()
+        {
+
+        }
+
+    }
+}
