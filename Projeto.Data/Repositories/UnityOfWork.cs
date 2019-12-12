0fb99b4d48530ac32c1bb1acb50db78be6d93ae7
marcio henrique <marcio.freitas@globalhitss.com.br>
2019-12-12 19:02:41 -0300

    Parte do Migrations Entity Framework - Márcio Henrique

--- Projeto.Data/Repositories/UnityOfWork.cs
+++ Projeto.Data/Repositories/UnityOfWork.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Projeto.Data.Context;
+using Projeto.Data.Contracts;
 
 namespace Projeto.Data.Repositories
 {
-    public class UnityOfWork
+    public class UnityOfWork : IUnityOfWork
     {
+        //atributo
+        private readonly DataContext context;
 
+        //construtor para injeção de dependência
+        public UnityOfWork(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IFornecedorRepository FornecedorRepository
+            => new FornecedorRepository(context);
+
+        public IProdutoRepository ProdutoRepository
+            => new ProdutoRepository(context);
     }
 }
