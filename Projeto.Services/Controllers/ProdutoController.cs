f52eee1244fdad3ca2ee5b76565c7878ac61ee4d
Marcio henrique <marcio.freitas@globalhitss.com.br>
2020-01-10 17:04:54 -0300

    Elaboração da API

---
+++ Projeto.Services/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto.Data.Entities;
+
+namespace Projeto.Services.Controllers
+{
+    
+    public class ProdutoController : ApiControllerAttribute 
+    {
+        //[HttpPost]
+        //[Route("Cadastrar")]
+        //public HttpResponseMessage Cadastro(Produto p)
+        //{
+            
+        //}
+    }
+}
